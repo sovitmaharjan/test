229b304cdf503d93c7e748b4fd26b0479a4ff0c8
sovitmaharjan <sovitmaharjan@gmail.com>
2022-06-23 14:42:12 +0545

    .

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NotificationTestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('send-notification', [NotificationTestController::class, 'sendnotification']);
