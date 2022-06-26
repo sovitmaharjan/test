77734ecb7ef50591a350938b092e92ae5934c2d5
sovitmaharjan <sovitmaharjan@gmail.com>
2022-06-26 21:29:17 +0545

    .

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/', function () {
+//     $a = DB::table('posts')
+//         ->whereBetween('created_at', ['2022-06-25', '2022-06-29'])
+//         ->count();
+
+//     $a = DB::select('select (select count(id) from posts where date(created_at) <= "2022-06-27" and date(created_at) >= "2022-06-25") as ongoing_event, (select count(id) from posts where date(created_at) <= "2022-06-27" and date(created_at) >= "2022-06-25") as total_event');
+
+//     $b = DB::table('posts')
+//         ->select(DB::raw('count(id) as ongoing_event'))
+//         ->where('created_at', '<=', "2022-06-27")
+//         ->where('created_at', '>=', "2022-06-25")
+//         ->get();
+//     dd(collect($a), $b);
+// });
+
 Route::resource('/post', PostController::class);
