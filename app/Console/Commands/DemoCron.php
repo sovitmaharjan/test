ce6a156058c62fbb40f67b69e1dafdeac3d1f4c3
sovitmaharjan <sovitmaharjan@gmail.com>
2022-09-12 13:13:59 +0545

    cron

---
+++ app/Console/Commands/DemoCron.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class DemoCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'demo:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Log::info("Cron is working fine!");
+     
+        /*
+           Write your database logic we bellow:
+           Item::create(['name'=>'hello new']);
+        */
+    }
+}
