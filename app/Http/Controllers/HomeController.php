0ad3810b06ad1d316f5c7a7a59c3c6860253fb5c
sovitmaharjan <sovitmaharjan@gmail.com>
2022-09-26 14:09:27 +0545

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,11 +11,23 @@ class HomeController extends Controller
 {
     public function notify()
     {
+        // $user = User::create([
+        //     'name' => 'name1',
+        //     'email' => 'name@name.com',
+        //     'email_verified_at' => now(),
+        //     'password' => '123'
+        // ]);
+        // $user2 = User::create([
+        //     'name' => 'name2',
+        //     'email' => 'name2@name.com',
+        //     'email_verified_at' => now(),
+        //     'password' => '123'
+        // ]);
+
         $user = User::where([
             'name' => 'name1',
             'email' => 'name@name.com',
         ])->first();
-
         $user2 = User::where([
             'name' => 'name2',
             'email' => 'name2@name.com',
