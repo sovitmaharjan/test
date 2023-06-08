7f6bc68a3fc11638b02e4e295e7ff7d6c1308949
--49 <sovitmaharjan@gmail.com>
2023-06-09 05:18:12 +0545

    test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    dd(User::all());
+    dd('ajklsdhflakjsdhfkalsfh');
     return view('welcome');
 });
