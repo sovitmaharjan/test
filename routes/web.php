5f7ab47617d42ea4cefd7de5c7b7dbffe9eba64e
--49 <sovitmaharjan@gmail.com>
2023-11-01 22:50:54 +0545

    .

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $response = Http::asForm()->post('http://127.0.0.1:8005/oauth/token', [
+        'grant_type' => 'client_credentials',
+        'client_id' => 2, // Replace with the actual client ID from the `id` column
+        'client_secret' => 'wyNGTgsMDYeCRYgiZJGRUdE5PjEzoXOctIT2q892', // Replace with the actual client secret from the `secret` column
+        'scope' => '*',
+    ]);
+    return $response->json();
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
